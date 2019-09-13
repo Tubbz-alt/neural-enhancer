1d14797ba43a62c8d34e1a7e0ecf8c89a8c92856
Gokhan Egri <egrigokhan@gmail.com>
2019-09-13 16:01:39 +0300

    Update model.py

--- lib/model.py
+++ lib/model.py
@@ -153,7 +153,8 @@ def test_data_loader(FLAGS):
 
     # Read in and preprocess the images
     def preprocess_test(name, mode):
-        im = sic.imread(name, mode="RGB").astype(np.float32)
+        im = matplotlib.pyplot.imread(name, mode="RGB").astype(np.float32)
+        # im = sic.imread(name, mode="RGB").astype(np.float32)
         # check grayscale image
         if im.shape[-1] != 3:
             h, w = im.shape
@@ -196,7 +197,8 @@ def inference_data_loader(FLAGS):
 
     # Read in and preprocess the images
     def preprocess_test(name):
-        im = sic.imread(name, mode="RGB").astype(np.float32)
+        im = matplotlib.pyplot.imread(name, mode="RGB").astype(np.float32)
+        # im = sic.imread(name, mode="RGB").astype(np.float32)
         # check grayscale image
         if im.shape[-1] != 3:
             h, w = im.shape
